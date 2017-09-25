b5e7fcd393110d900cd3d47c2e21cbc75d71ae70
TaeJeong Kwon <TaeJeong Kwon>
2017-09-25 23:46:23 +0900

    초기 커밋.

---
+++ NumberBaseball/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace NumberBaseball
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_number = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listnumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.liststrike = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listball = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // tb_number
+            // 
+            this.tb_number.Location = new System.Drawing.Point(12, 50);
+            this.tb_number.Name = "tb_number";
+            this.tb_number.Size = new System.Drawing.Size(166, 25);
+            this.tb_number.TabIndex = 0;
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(201, 50);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 42);
+            this.btn_ok.TabIndex = 1;
+            this.btn_ok.Text = "확인";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listnumber,
+            this.liststrike,
+            this.listball});
+            this.listView1.Location = new System.Drawing.Point(12, 98);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(406, 318);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // listnumber
+            // 
+            this.listnumber.Text = "숫자";
+            this.listnumber.Width = 109;
+            // 
+            // liststrike
+            // 
+            this.liststrike.Text = "스트라이크";
+            this.liststrike.Width = 107;
+            // 
+            // listball
+            // 
+            this.listball.Text = "볼";
+            this.listball.Width = 90;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 428);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.tb_number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_number;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader listnumber;
+        private System.Windows.Forms.ColumnHeader liststrike;
+        private System.Windows.Forms.ColumnHeader listball;
+    }
+}
+
---
+++ NumberBaseball/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NumberBaseball
+{
+    public partial class Form1 : Form
+    {
+        private int number = 0;
+        private int[] numbersInts = new int[3];
+        int[] insertInts = new int[3];
+        int strike;
+        int ball;
+        public Form1()
+        {
+            InitializeComponent();
+            this.RandomGetNumberMethod();
+        }
+
+        private void RandomGetNumberMethod()
+        {
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int randomNumber = 0;
+                if (i == 0)
+                {
+                    randomNumber = random.Next(1, 9);
+                }
+                else
+                {
+                    randomNumber = random.Next(0, 9);
+                }
+                numbersInts[i] = randomNumber;
+
+                if (0 < i)
+                {
+                    var intersection = numbersInts.Where(w => w == randomNumber).ToArray();
+                    if (intersection.Length > 0)
+                    {
+                        i--;
+                    }
+                }
+            }
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            if (tb_number.Text.Length == 3)
+            {
+                number = int.Parse(tb_number.Text.ToString());
+
+                insertInts[0] = number / 100;
+                insertInts[1] = (number % 100) / 10;
+                insertInts[2] = (number % 100) % 10;
+
+                this.CompareMethod();
+            }
+            else
+            {
+                MessageBox.Show("3자리 숫자를 입력해 주세요.");
+            }
+        }
+
+        private void CompareMethod()
+        {
+            strike = 0;
+            ball = 0;
+            
+            for(int i = 0; i< 3; i++)
+            {
+                if(insertInts[i] == numbersInts[i])
+                {
+                    strike++;
+                }
+                else
+                {
+                    var ballnumber = numbersInts.Where(w => w == insertInts[i]).ToArray();
+                    if(ballnumber != null)
+                    {
+                        ball++;
+                    }
+                }
+            }
+            listView1.Items.Add(number.ToString());
+            listView1.Items.Add(strike.ToString());
+            listView1.Items.Add(ball.ToString());
+        }
+    }
+}
